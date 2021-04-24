d08596de933cd311e60e2b4216c97f90dd4f54f4
zacharyhester2 <zachary.hester2@gmail.com>
2021-04-24 13:41:07 -0500

    sample API for Nasa, Smith, News

--- server/app.js
+++ server/app.js
@@ -1,12 +1,63 @@
 const path = require('path');
 const express = require('express');
-
-
 const CLIENT_PATH = path.resolve(__dirname, '../client/dist');
 const app = express();
 app.use(express.json());
+const axios = require('axios');
+require('dotenv').config()
 
 app.use(express.static(CLIENT_PATH));
+
+//KEYS
+const newsKey = process.env.NEWS_KEY;
+const smithKey = process.env.SMITH_KEY;
+const nasaKey = process.env.NASA_KEY;
+// const youtubeApi = process.env.YOUTUBE_API_KEY;
+
+// NASA PotD - return title, url, explanation (SOMETIMES VIDEO)
+const nasaPotD = `https://api.nasa.gov/planetary/apod?api_key=${nasaKey}`;
+
+app.get('/nasaPic', (req, res) => {
+  
+    axios.get(nasaPotD)
+    .then(({data: {title, url, explanation}}) =>{
+      res.status(200).send({title, url, explanation});
+    })
+    .catch((err) => {
+      res.status(500).send(err);
+    });
+});      
+
+//SMITHSONIAN SEARCH - *very little documentation on API*
+
+const smithQ = 'iris';  //hard search data, will change once front end
+const smith = `https://api.si.edu/openaccess/api/v1.0/search?q=${smithQ}&api_key=${smithKey}`;
+
+app.get('/smithQ/:search', (req, res) => {
+  
+    axios.get(smith)
+    .then(({data}) =>{
+      res.status(200).send({data});
+    })
+    .catch((err) => {
+      res.status(500).send(err);
+    });
+});     
+
+const newsQ = 'the lost cosmonauts';
+const sortBy = 'popularity' //maybe give users the options: relevancy, popularity, publishedAt
+const news = `https://newsapi.org/v2/everything?q=${newsQ}&apiKey=${newsKey}&sortBy=${sortBy}`;
+
+app.get('/newsQ/:search', (req, res) => {
+  
+    axios.get(news)
+    .then(({data}) =>{
+      res.status(200).send(data);
+    })
+    .catch((err) => {
+      res.status(500).send(err);
+    });
+});   
 
 
 module.exports = {
