17360304d7060e661a06671b424c38f9ea7376c1
zacharyhester2 <zachary.hester2@gmail.com>
2021-04-24 17:28:29 -0500

    Saturday DB Connected

--- server/database/schema.js
+++ server/database/schema.js
@@ -1,68 +1,72 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-const userSchema = mongoose.Schema({
-  id: String,
-  name: String,
-  avatar: Object,
-  cloudinaryId: String,
-  notifs: Array,
-  interests: Array,
-  stamps: Array,
-  badges: Array,
-});
+// const userSchema = mongoose.Schema({
+//   id: String,
+//   name: String,
+//   avatar: Object,
+//   cloudinaryId: String,
+//   notifs: Array,
+//   interests: Array,
+//   stamps: Array,
+//   badges: Array,
+//   badgesProgress: {
+//       type: Map, 
+//       of: String
+// },
+// });
 
-const Users = mongoose.model('Users', userSchema);
+// const Users = mongoose.model('Users', userSchema);
 
-const docSchema = mongoose.Schema({
-  id: Number,
-  type: String,
-  title: String,
-  director: String,
-  image: String,
-  url: String,
-});
+// const docSchema = mongoose.Schema({
+//   id: Number,
+//   type: String,
+//   title: String,
+//   director: String,
+//   image: String,
+//   url: String,
+// });
 
-const Doc = mongoose.model('Doc', docSchema);
+// const Doc = mongoose.model('Doc', docSchema);
 
-const podSchema = mongoose.Schema({
-  id: Number,
-  type: String, 
-  title: String,
-  writer: String,
-  image: String,
-  url: String,
-});
+// const podSchema = mongoose.Schema({
+//   id: Number,
+//   type: String, 
+//   title: String,
+//   writer: String,
+//   image: String,
+//   url: String,
+// });
   
-const Pod = mongoose.model('Pod', podSchema);
+// const Pod = mongoose.model('Pod', podSchema);
 
-const newsSchema = mongoose.Schema({
-  id: Number,
-  type: String, 
-  title: String,
-  author: String,
-  image: String,
-  url: String,
-});
+// const newsSchema = mongoose.Schema({
+//   id: Number,
+//   type: String, 
+//   title: String,
+//   author: String,
+//   image: String,
+//   url: String,
+// });
   
-const News = mongoose.model('News', newsSchema);
+// const News = mongoose.model('News', newsSchema);
 
-const stampSchema = mongoose.Schema({
-  id: String,
-});
+// const stampSchema = mongoose.Schema({
+//   id: String,
+// });
 
-const Stamp = mongoose.model('Stamp', stampSchema);
+// const Stamp = mongoose.model('Stamp', stampSchema);
 
-const badgeSchema = mongoose.Schema({
-  id: String
-});
+// const badgeSchema = mongoose.Schema({
+//   id: String,
+// });
 
-const Badge = mongoose.model('Badge', badgeSchema);
+// const Badge = mongoose.model('Badge', badgeSchema);
 
-module.exports = {
-  Users,
-  Doc,
-  Pod,
-  News,
-  Stamp,
-  Badge
-};
+// module.exports = {
+//   Users,
+//   Doc,
+//   Pod,
+//   News,
+//   Stamp,
+//   Badge
+// };
