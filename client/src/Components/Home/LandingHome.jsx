d43195a79f09b155742ad27e617076c0f97493c1
zacharyhester2 <zachary.hester2@gmail.com>
2021-05-15 15:42:13 -0500

    pulling duplicates

--- client/src/Components/Home/LandingHome.jsx
+++ client/src/Components/Home/LandingHome.jsx
@@ -8,6 +8,7 @@ const LandingHomeStyled = styled.div`
   width: 100%;
   background-image: url('https://img.rawpixel.com/s3fs-private/rawpixel_images/website_content/pdmet-julian-03-gloy_0.jpg?w=800&dpr=1&fit=default&crop=default&q=65&vib=3&con=3&usm=15&bg=F4F4F3&ixlib=js-2.2.1&s=91f1263a708e7512549c8d165dc59c73');
   background-repeat: no-repeat;
+  background-attachment: fixed;
   background-size: 100%;
   margin-bottom: 40rem;
 `
